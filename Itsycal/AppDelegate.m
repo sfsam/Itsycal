a5d0feedb08a11a403e5c59bc1065b6c4d9f8eae
Sanjay Madan <sanjayma@gmail.com>
2016-07-25 10:46:55 -0700

    Ensure run from /Applications on 10.12+

--- Itsycal/AppDelegate.m
+++ Itsycal/AppDelegate.m
@@ -39,6 +39,14 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    // On macOS 10.12+, ensure the user has moved Itsycal to the
+    // /Applications folder. Having the user manually move Itsycal
+    // to /Applications turns off Gatekeeper Path Randomization
+    // and allows Itsycal to be updated with Sparkle. :P
+    if (OSVersionIsAtLeast(10, 12, 0)) {
+        [self checkIfRunFromApplicationsFolder];
+    }
+    
     // Register keyboard shortcut.
     [[MASShortcutBinder sharedBinder] bindShortcutWithDefaultsKey:kKeyboardShortcut toAction:^{
          [_vc keyboardShortcutActivated];
@@ -67,6 +75,29 @@
     [self removeMenuExtra];
     [_vc removeStatusItem];
     [[MASShortcutMonitor sharedMonitor] unregisterAllShortcuts];
+}
+
+#pragma mark -
+#pragma mark Applications folder check
+
+- (void)checkIfRunFromApplicationsFolder
+{
+    NSString *bundlePath = [[NSBundle mainBundle] bundlePath];
+    NSArray *applicationDirs = NSSearchPathForDirectoriesInDomains(NSApplicationDirectory, NSLocalDomainMask, YES);
+    for (NSString *appDir in applicationDirs) {
+        if ([bundlePath hasPrefix:appDir]) {
+            return; // Ok, Itsycal is being run from /Applications.
+        }
+    }
+    // Itsycal is not being run from /Applications.
+    [[NSApplication sharedApplication] activateIgnoringOtherApps:YES];
+    NSAlert *alert = [NSAlert new];
+    alert.messageText = NSLocalizedString(@"Move Itsycal to the Applications folder", nil);
+    alert.informativeText = NSLocalizedString(@"Itsycal must be run from the Applications folder in order to work properly.\n\nPlease quit Itsycal, move it to the Applications folder, and relaunch.", nil);
+    alert.icon = [NSImage imageNamed:@"move"];
+    [alert addButtonWithTitle:NSLocalizedString(@"Quit Itsycal", @"")];
+    [alert runModal];
+    [NSApp terminate:nil];
 }
 
 #pragma mark -
