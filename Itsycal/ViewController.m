cd143030ba7547379df3666962531e2cee8c61a8
Sanjay Madan <sanjayma@gmail.com>
2016-10-30 16:16:18 -0700

    FIX: Remember position in menu bar

--- Itsycal/ViewController.m
+++ Itsycal/ViewController.m
@@ -350,6 +350,10 @@
     _statusItem.button.target = self;
     _statusItem.button.action = @selector(statusItemClicked:);
     _statusItem.highlightMode = NO; // Deprecated in 10.10, but what is alternative?
+    // Remember item position in menubar for 10.12+. (@pskowronek (Github))
+    if (OSVersionIsAtLeast(10, 12, 0)) {
+        [_statusItem setAutosaveName:@"ItsycalStatusItem"];
+    }
     [self updateMenubarIcon];
     [self updateStatusItemPositionInfo];
     [self.itsycalWindow positionRelativeToRect:_menuItemFrame screenFrame:_screenFrame];
@@ -364,8 +368,13 @@
     if (_statusItem) {
         [[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowDidMoveNotification object:_statusItem.button.window];
         [[NSNotificationCenter defaultCenter] removeObserver:self name:NSWindowDidResizeNotification object:_statusItem.button.window];
-        [[NSStatusBar systemStatusBar] removeStatusItem:_statusItem];
-        _statusItem = nil;
+        // Let 10.12+ remember item position and remove item when app is terminated.
+        // If we remove item ourselves, autosavename is deleted from user defaults.
+        // (@pskowronek (Github))
+        if (!OSVersionIsAtLeast(10, 12, 0)) {
+            [[NSStatusBar systemStatusBar] removeStatusItem:_statusItem];
+            _statusItem = nil;
+        }
     }
 }
 
