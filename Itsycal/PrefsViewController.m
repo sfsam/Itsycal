38c67ad38bf18a8063b1c0714e17003b07164a48
Sanjay Madan <sanjayma@gmail.com>
2016-12-09 11:40:11 -0800

    MoLoginItem framework (quiets LSSharedFileList deprecation warnings)

--- Itsycal/PrefsViewController.m
+++ Itsycal/PrefsViewController.m
@@ -10,6 +10,7 @@
 #import "Itsycal.h"
 #import "MASShortcut/MASShortcutView.h"
 #import "MASShortcut/MASShortcutView+Bindings.h"
+#import "MoLoginItem/MoLoginItem.h"
 #import "MoView.h"
 #import "MoTextField.h"
 #import "EventCenter.h"
@@ -205,8 +206,8 @@ static NSString * const kCalendarCellId = @"CalendarCell";
 {
     [super viewWillAppear];
     [_calendarsTV reloadData];
-    _login.state = [self isLoginItemEnabled] ? NSOnState : NSOffState;
-    
+    _login.state = MOIsLoginItemEnabled() ? NSOnState : NSOffState;
+
     // Bindings for icon preferences
     [_useOutlineIcon bind:@"value" toObject:[NSUserDefaultsController sharedUserDefaultsController] withKeyPath:[@"values." stringByAppendingString:kUseOutlineIcon] options:@{NSContinuouslyUpdatesValueBindingOption: @(YES)}];
     [_showMonth bind:@"value" toObject:[NSUserDefaultsController sharedUserDefaultsController] withKeyPath:[@"values." stringByAppendingString:kShowMonthInIcon] options:@{NSContinuouslyUpdatesValueBindingOption: @(YES)}];
@@ -239,58 +240,7 @@ static NSString * const kCalendarCellId = @"CalendarCell";
 
 - (void)launchAtLogin:(NSButton *)login
 {
-    [self enableLoginItem:login.state == NSOnState];
-}
-
-- (BOOL)isLoginItemEnabled
-{
-    BOOL isEnabled = NO;
-    NSString *appPath = [[NSBundle mainBundle] bundlePath];
-    LSSharedFileListRef loginItemsRef = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
-    
-    if (loginItemsRef) {
-        UInt32 seedValue;
-        NSArray *loginItems = CFBridgingRelease(LSSharedFileListCopySnapshot(loginItemsRef, &seedValue));
-        for (id item in loginItems) {
-            LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)item;
-            NSURL *pathURL = CFBridgingRelease(LSSharedFileListItemCopyResolvedURL(itemRef, 0, NULL));
-            if (pathURL && [pathURL.path hasPrefix:appPath]) {
-                isEnabled = YES;
-                break;
-            }
-        }
-        CFRelease(loginItemsRef);
-    }
-    return isEnabled;
-}
-
-- (void)enableLoginItem:(BOOL)enable;
-{
-    NSString *appPath = [[NSBundle mainBundle] bundlePath];
-    LSSharedFileListRef loginItemsRef = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
-    
-    if (loginItemsRef) {
-        if (enable) {
-            // We call LSSharedFileListInsertItemURL to insert the item at the bottom of Login Items list.
-            CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:appPath];
-            LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItemsRef, kLSSharedFileListItemLast, NULL, NULL, url, NULL, NULL);
-            if (item != NULL) CFRelease(item);
-        }
-        else {
-            // Grab the contents of the shared file list (LSSharedFileListItemRef objects)
-            // and pop it in an array so we can iterate through it to find our item.
-            UInt32 seedValue;
-            NSArray *loginItems = CFBridgingRelease(LSSharedFileListCopySnapshot(loginItemsRef, &seedValue));
-            for (id item in loginItems) {
-                LSSharedFileListItemRef itemRef = (__bridge LSSharedFileListItemRef)item;
-                NSURL *pathURL = CFBridgingRelease(LSSharedFileListItemCopyResolvedURL(itemRef, 0, NULL));
-                if (pathURL && [pathURL.path hasPrefix:appPath]) {
-                    LSSharedFileListItemRemove(loginItemsRef, itemRef); // Deleting the item
-                }
-            }
-        }
-        CFRelease(loginItemsRef);
-    }
+    MOEnableLoginItem(login.state == NSOnState);
 }
 
 #pragma mark -
