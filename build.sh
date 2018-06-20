007977c3de529050dfc2ff32aeff5649d6bcd9c2
Sanjay Madan <sanjayma@gmail.com>
2018-06-20 14:36:32 -0700

    build.sh uses CFBundleShortVersionString

--- build.sh
+++ build.sh
@@ -8,11 +8,15 @@
 
 # Get the bundle version from the plist.
 PLIST_FILE="Itsycal/Info.plist"
-VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $PLIST_FILE)
+VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" $PLIST_FILE)
+SHORT_VERSION_STRING=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $PLIST_FILE)
 
 # Set up file names and paths.
+# The short version string might have spaces,
+# so replace those with -.
 BUILD_PATH=$(mktemp -d "$TMPDIR/Itsycal.XXXXXX")
-ZIP_NAME="Itsycal-$VERSION.zip"
+ZIP_NAME="Itsycal-$SHORT_VERSION_STRING.zip"
+ZIP_NAME=${ZIP_NAME// /-}
 ZIP_PATH1="$HOME/Desktop/$ZIP_NAME"
 ZIP_PATH2="$HOME/Desktop/Itsycal.zip"
 XML_PATH="$HOME/Desktop/itsycal.xml"
@@ -46,13 +50,14 @@ cat > "$XML_PATH" <<EOF
 <description>Most recent changes</description>
 <language>en</language>
 <item>
-<title>Version $VERSION</title>
+<title>Version $SHORT_VERSION_STRING</title>
 <sparkle:minimumSystemVersion>10.12</sparkle:minimumSystemVersion>
 <sparkle:releaseNotesLink>https://s3.amazonaws.com/itsycal/changelog.html</sparkle:releaseNotesLink>
 <pubDate>$DATE +0000</pubDate>
 <enclosure
     url="https://s3.amazonaws.com/itsycal/$ZIP_NAME"
     sparkle:version="$VERSION"
+    sparkle:shortVersionString="$SHORT_VERSION_STRING"
     length="$FILESIZE"
     type="application/octet-stream" />
 </item>
