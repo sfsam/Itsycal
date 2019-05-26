c615d5f774323562c674de54dedd8fc43e14d287
Sanjay Madan <sanjayma@gmail.com>
2019-05-26 12:05:11 -0700

    Prevent retain cycle in EventCenter

--- Itsycal/EventCenter.m
+++ Itsycal/EventCenter.m
@@ -53,12 +53,15 @@ static NSString *kSelectedCalendars = @"SelectedCalendars";
         }];
 
         // Refetch everything when the event store has changed.
+        // Use weakSelf so -dealloc can be called after user calls
+        // -resetEventCenter in ViewController.
+        __weak __typeof(self) weakSelf = self;
         [[NSNotificationCenter defaultCenter] addObserverForName:EKEventStoreChangedNotification object:_store queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
             // Coalesce notifications arriving within 1 second of
             // each other because some actions, like creating or
             // deleting events, generate multiple notifications.
-            [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(refetchAll) object:nil];
-            [self performSelector:@selector(refetchAll) withObject:nil afterDelay:1];
+            [NSObject cancelPreviousPerformRequestsWithTarget:weakSelf selector:@selector(refetchAll) object:nil];
+            [weakSelf performSelector:@selector(refetchAll) withObject:nil afterDelay:1];
         }];
     }
     return self;
