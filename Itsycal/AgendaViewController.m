950a4d89a3e862e6b7bcd140bfe44056ae28b101
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-12-06 17:42:39 -0300

    Fix event table not occupying full width on Big Sur

--- Itsycal/AgendaViewController.m
+++ Itsycal/AgendaViewController.m
@@ -82,6 +82,9 @@ static NSString *kEventCellIdentifier = @"EventCell";
     _tv.refusesFirstResponder = YES;
     _tv.dataSource = self;
     _tv.delegate = self;
+    if (@available(macOS 11.0, *)) {
+        _tv.style = NSTableViewStylePlain;
+    }
     [_tv addTableColumn:[[NSTableColumn alloc] initWithIdentifier:kColumnIdentifier]];
     
     // Calendars enclosing scrollview
