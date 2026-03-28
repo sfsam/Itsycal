f402139895b3dcfa8d7922ad874b06e49039d9de
Juan Manso <jmanso@quantinium.cc>
2026-03-28 18:24:07 +0100

    feat: add kShowEventCountdown preference key
    Add kShowEventCountdown preference key.
    Add NSUserDefaults key to control the event countdown
    feature in the menubar.

--- Itsycal/Itsycal.m
+++ Itsycal/Itsycal.m
@@ -32,3 +32,4 @@ NSString * const kDoNotDrawOutlineAroundCurrentMonth = @"DoNotDrawOutlineAroundC
 NSString * const kShowDaysWithNoEventsInAgenda = @"ShowDaysWithNoEventsInAgenda";
 NSString * const kShowEventPopoverOnHover = @"ShowEventPopoverOnHover";
 NSString * const kEnableTahoeMenuIcons = @"EnableTahoeMenuIcons";
+NSString * const kShowEventCountdown = @"ShowEventCountdown";
