506b176f894eff4214a0bda6457dc9fb82c10884
Sanjay <sanjayma@gmail.com>
2026-09-13 15:33:56 -0700

    Fix dim menu bar text on inactive displays

--- Itsycal/ViewController.m
+++ Itsycal/ViewController.m
@@ -707,7 +707,17 @@ static NSArray<NSString *> *ObservedDefaultsKeys(void)
             // Prepend a space to _clockFormat text to separate it from icon.
             buttonText = [@" " stringByAppendingString:buttonText];
         }
-        _statusItem.button.attributedTitle = [[NSAttributedString alloc] initWithString:buttonText attributes:@{NSBaselineOffsetAttributeName: @(baselineOffset)}];
+        // Start with AppKit's default status-item title attributes so its
+        // dynamic foreground color follows the active menu bar on each screen.
+        // Creating an attributed string with only a baseline offset causes
+        // the text to default to black, making it illegible on inactive
+        // displays with a dark menu bar.
+        _statusItem.button.title = buttonText;
+        NSMutableAttributedString *attributedTitle = _statusItem.button.attributedTitle.mutableCopy;
+        [attributedTitle addAttribute:NSBaselineOffsetAttributeName
+                                value:@(baselineOffset)
+                                range:NSMakeRange(0, attributedTitle.length)];
+        _statusItem.button.attributedTitle = attributedTitle;
     }
     _statusItem.button.accessibilityTitle = accessibilityTitle;
     [self adjustStatusItemWidthIfNecessary];
@@ -1500,4 +1510,3 @@ static NSArray<NSString *> *ObservedDefaultsKeys(void)
 }
 
 @end
-
